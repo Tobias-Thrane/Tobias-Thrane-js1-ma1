fb760f49291635c1a05179f55decf40d46df8edd
Tobias Thrane <tkthrane_93@hotmail.com>
2020-03-29 12:07:22 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,141 @@
+// question 1
+
+//var cat = {
+//    complain: "Meow!"
+//}
+
+// question 2
+
+//console.dir(document.querySelector("h3"));
+
+// question 3
+
+//const firsth3 = document.querySelector("h3");
+//firsth3.style.fontSize = "2em"
+
+// question 4
+
+//document.querySelector("h3").classList.add("subheading");
+
+// question 5
+// var paragraphs = document.querySelectorAll("p");
+
+// question 6
+// var resultsContainer = document.querySelector(".results");
+// document.querySelector(".results").innerHTML = "<p>New paragraph</p>"
+
+// question 7
+
+//function go (catArray) {
+//    const cats = [
+//        {
+//            name: "Blob",
+//            age: 10
+//        },
+//        {
+//            name: "Harold",
+//        },
+//        {
+//            name: "Blurt",
+//            age: 21
+//        }
+//    ];
+//    for(var i = 0; i < cats.length; i++) {
+//
+//       
+//        console.log(cats[i].name);
+//    }
+//
+//}
+//
+//go();
+
+// question 8
+
+//function go (catArray) {
+//    const cats = [
+//        {
+//            name: "Blob",
+//            age: 10
+//        },
+//        {
+//            name: "Harold",
+//        },
+//        {
+//            name: "Blurt",
+//            age: 21
+//        }
+//    ];
+//
+//    
+//
+//    for (let i = 0; i < cats.length; i++) {
+//     
+//        const name = "<h5>" + cats[i].name + "</h5>";
+//        console.log(name);
+//    }
+//    
+//}
+//
+//go();
+
+// question 9
+
+//function go (catArray) {
+//    const cats = [
+//        {
+//            name: "Blob",
+//            age: 10
+//        },
+//        {
+//            name: "Harold",
+//        },
+//        {
+//            name: "Blurt",
+//            age: 21
+//        }
+//    ];
+//
+//    
+//
+//    for (let i = 0; i < cats.length; i++) {
+//     
+//        const name = "<h5>" + cats[i].name + "</h5>";
+//        console.log(name);
+//    }
+//    var resultsContainer = document.querySelector(".results");
+//    document.querySelector(".results").innerHTML = "<p>New paragraph</p>"
+//}
+//
+//go();
+
+// question 10
+
+//function go (catArray) {
+//        const cats = [
+//            
+//            {
+//                name: "Blob",
+//                age: 10
+//            },
+//            {
+//                name: "Harold",
+//            },
+//            {
+//                name: "Blurt",
+//                age: 21
+//            }
+//            
+//            
+//        ];
+//        for (let i = 0; i < cats.length; i++) {
+//            
+//            
+//            const name = '<div>'+"<h5>" + cats[i].name + "</h5>"
+//            console.log(name);
+//            const age = "<p>" + cats[i].age + "</p>"+'</div>'
+//            console.log(age);
+//        }
+//    }
+//    go();
+
